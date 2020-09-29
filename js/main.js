a50f85262e27ea98139e5d7c299abceead25bbc6
thiago <hollebenthiago@gmail.com>
2020-09-28 23:31:29 -0300

    addition of points

--- js/main.js
+++ js/main.js
@@ -15,12 +15,23 @@ function multiplication() {
     let n = parseInt(document.getElementById('n').value);
     C = new Curve(p, a, b);
     P = new Point(x1,y1,z1,C);
-    console.log(P)
-    Q = mult(n, P)
-    console.log(p,a,b,x1,y1,z1,n);
-    console.log(C);
-    console.log(P);
-    console.log(Q);
-    // alert('The coordinates of '.concat((n).toString(), 'P' ,' are: ', (Q.x).toString(), ', ', (Q.y).toString(), ', ', (Q.z).toString()))
+    Q = mult(n, P);
+    document.getElementById('result').innerHTML = 'The coordinates of '.concat((n).toString(), 'P' ,' are: [', (Q.x).toString(), ': ', (Q.y).toString(), ': ', (Q.z).toString(), ']')
+}
+
+function addition() {
+    let p = parseInt(document.getElementById('prime').value);
+    let a = parseInt(document.getElementById('aParam').value);
+    let b = parseInt(document.getElementById('bParam').value);
+    let x1 = parseInt(document.getElementById('xcoord1').value);
+    let y1 = parseInt(document.getElementById('ycoord1').value);
+    let z1 = parseInt(document.getElementById('zcoord1').value);
+    let x2 = parseInt(document.getElementById('xcoord2').value);
+    let y2 = parseInt(document.getElementById('ycoord2').value);
+    let z2 = parseInt(document.getElementById('zcoord2').value);
+    C = new Curve(p, a, b);
+    P = new Point(x1,y1,z1,C);
+    Q = new Point(x2,y2,z2,C);
+    R = addPoints(P, Q);
     document.getElementById('result').innerHTML = 'The coordinates of '.concat((n).toString(), 'P' ,' are: [', (Q.x).toString(), ': ', (Q.y).toString(), ': ', (Q.z).toString(), ']')
 }
