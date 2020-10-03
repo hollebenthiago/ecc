0903241236edb3043b0f3916470c315476aa4892
thiago <hollebenthiago@gmail.com>
2020-10-03 18:28:10 -0300

    fixing typo

--- js/curve.js
+++ js/curve.js
@@ -5,7 +5,7 @@ function Curve(p, a, b) {
     
     this.includePoint = function(x, y, z) {
     
-        if ((z*y**2 - z*x**3 - z**2*this.equation[0]*x - z**3*this.equation[1]) % this.p == 0) {
+        if ((z*y**2 - x**3 - z**2*this.equation[0]*x - z**3*this.equation[1]) % this.p == 0) {
             return true
         }
     
