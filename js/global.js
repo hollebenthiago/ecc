4edc8ae3bc6c2444cf24fc849583185867271021
thiago <hollebenthiago@gmail.com>
2020-10-14 00:28:10 -0300

    fixing order of scripts

---
+++ js/global.js
@@ -0,0 +1,4 @@
+// curve used for cryptography
+const E = new Curve(16777259, 3, 7, true);
+const cardinality = 16773749;
+const basePoint = new Point(BigInt(2699101), BigInt(9795704), BigInt(1), E);
